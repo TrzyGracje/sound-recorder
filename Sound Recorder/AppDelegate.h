ce8c6c3c7e9d728e95f53cbb46a80f6fa239b80a
Lukasz Komorowski <komo.luka@gmail.com>
2016-05-29 16:38:35 +0200

    Initial Commit

---
+++ Sound Recorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sound Recorder
+//
+//  Created by Lukasz Komorowski on 29.05.2016.
+//  Copyright © 2016 Lukasz Komorowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sound Recorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sound Recorder
+//
+//  Created by Lukasz Komorowski on 29.05.2016.
+//  Copyright © 2016 Lukasz Komorowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
